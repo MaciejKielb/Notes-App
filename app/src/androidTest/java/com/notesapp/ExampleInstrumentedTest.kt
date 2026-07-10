640d780e557a3ca0dbfaccc33dc8743ae0d35171
Maciej Kielb <maciejkielb@gmail.com>
2026-07-10 14:00:23 +0300

    small changes around naming v2

--- app/src/androidTest/java/com/notesapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/notesapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.notesApplication", appContext.packageName)
+        assertEquals("com.notesapp", appContext.packageName)
     }
 }
