d6eed3076910fe4578fdfa036c91ca5da3b164dc
Maciej Kielb <maciejkielb@gmail.com>
2026-07-22 12:32:44 +0300

    app entry point, room dependencies

---
+++ app/src/main/java/com/notesapp/NotesApplication.kt
@@ -0,0 +1,17 @@
+package com.notesapp
+
+import android.app.Application
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class NotesApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@NotesApplication)
+            //modules(appModule)
+        }
+
+    }
+}
