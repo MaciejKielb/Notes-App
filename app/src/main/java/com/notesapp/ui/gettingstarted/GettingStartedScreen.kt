b8d58749c1fc55a30601514c7483c974e0399b86
Maciej Kielb <maciejkielb@gmail.com>
2026-09-07 12:04:05 +0200

    add page indicator

--- app/src/main/java/com/notesapp/ui/gettingstarted/GettingStartedScreen.kt
+++ app/src/main/java/com/notesapp/ui/gettingstarted/GettingStartedScreen.kt
@@ -2,7 +2,9 @@ package com.notesapp.ui.gettingstarted
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,9 +13,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -70,6 +75,27 @@ fun GettingStartedContent(
             verticalAlignment = Alignment.Top,
         ) { position ->
             PagerScreen(onBoardingPage = pages[position])
+        }
+        Row(
+            modifier =
+                Modifier
+                    .weight(1.5f)
+                    .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.DarkGray
+                    else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp),
+                )
+            }
         }
         FinishButton(
             modifier = Modifier.weight(2f),
